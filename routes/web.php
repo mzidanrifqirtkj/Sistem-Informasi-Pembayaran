3d3f2b819296a015f1ec93c2389286cecd09e678
mzidanrifqirtkj <mzidanrifqirtkj@gmail.com>
2025-01-05 10:15:09 +0700

    feat:layout, dashboard, santri, profile

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
-use App\Http\Controllers\SantriController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\SantriController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/data-santri', [SantriController::class, 'showSantri']);
+// Route::get('/data-santri', [SantriController::class, 'showSantri']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('admin.profile');
+
+Route::get('/data_santri', [SantriController::class, 'index'])->name('admin.data_santri');
+
+
+
