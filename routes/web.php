970917bbede8d1c10981d0ad79c21ebc398f0dbb
Wahdie <87081415+Wahdie@users.noreply.github.com>
2025-01-06 10:32:49 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+Route::get('/profile', [ProfileController::class, 'index'])->name('admin.profile');
+Route::get('/data_santri', [SantriController::class, 'index'])->name('admin.data_santri');
+
+
 Route::get('/santri/import', [SantriController::class, 'importForm'])->name('santri.importForm');
 Route::post('/santri/import', [SantriController::class, 'import'])->name('santri.import');
-
 Route::resource('santri', SantriController::class);
-
 
 
