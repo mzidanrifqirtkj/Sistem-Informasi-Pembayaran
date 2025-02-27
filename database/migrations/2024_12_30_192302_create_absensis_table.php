7b1872bb8f898995345424178e6c386bfddc741e
mzidanrifqirtkj <mzidanrifqirtkj@gmail.com>
2025-02-27 14:37:30 +0700

    feat:add seeder Absensi

--- database/migrations/2024_12_30_192302_create_absensis_table.php
+++ database/migrations/2024_12_30_192302_create_absensis_table.php
@@ -9,40 +9,22 @@ class CreateAbsensisTable extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('absensis', function (Blueprint $table) {
             $table->id('id_absensi');
-            $table->unsignedBigInteger('santri_id');
-            $table->tinyInteger('jumlah_hadir')->default(0);
-            $table->tinyInteger('jumlah_izin')->default(0);
-            $table->tinyInteger('jumlah_sakit')->default(0);
-            $table->tinyInteger('jumlah_alpha')->default(0);
-            $table->enum('bulan', [
-                'Jan',
-                'Feb',
-                'Mar',
-                'Apr',
-                'May',
-                'Jun',
-                'Jul',
-                'Aug',
-                'Sep',
-                'Oct',
-                'Nov',
-                'Dec'
-            ])->default('Jan');
-
-            // Enum untuk minggu dalam bulan
-            $table->enum('minggu_per_bulan', ['Minggu 1', 'Minggu 2', 'Minggu 3', 'Minggu 4', 'Minggu 5']);
-            $table->unsignedBigInteger('tahun_ajar_id');
-            $table->unsignedBigInteger('kelas_id');
+            $table->string('nis', 50);
+            $table->tinyInteger('jumlah_hadir')->unsigned()->default(0);
+            $table->tinyInteger('jumlah_izin')->unsigned()->default(0);
+            $table->tinyInteger('jumlah_sakit')->unsigned()->default(0);
+            $table->tinyInteger('jumlah_alpha')->unsigned()->default(0);
+            $table->enum('bulan', ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])->default('Jan');
+            $table->enum('minggu_per_bulan', ['Minggu 1', 'Minggu 2', 'Minggu 3', 'Minggu 4', 'Minggu 5'])->nullable();
+            $table->foreignId('tahun_ajar_id')->constrained('tahun_ajars')->cascadeOnDelete();
+            $table->foreignId('kelas_id')->constrained('kelas')->cascadeOnDelete();
             $table->timestamps();
 
-            // Foreign Key Constraints
-            $table->foreign('santri_id')->references('id')->on('santris')->onDelete('cascade');
-            $table->foreign('tahun_ajar_id')->references('id')->on('tahun_ajar')->onDelete('cascade');
-            $table->foreign('kelas_id')->references('id')->on('kelas')->onDelete('cascade');
+            $table->foreign('nis')->references('nis')->on('santris')->cascadeOnDelete();
         });
     }
 
--- database/seeders/AbsensiSeeder.php
+++ database/seeders/AbsensiSeeder.php
@@ -1,17 +1,52 @@
 <?php
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AbsensiSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('absensis')->insert([
+            [
+                'nis' => '201001',
+                'jumlah_hadir' => 3,
+                'jumlah_izin' => 30,
+                'jumlah_sakit' => 10,
+                'jumlah_alpha' => 30,
+                'bulan' => 'Sep',
+                'minggu_per_bulan' => 'Minggu 2',
+                'tahun_ajar_id' => 8,
+                'kelas_id' => 5,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nis' => '1004',
+                'jumlah_hadir' => 1,
+                'jumlah_izin' => 1,
+                'jumlah_sakit' => 1,
+                'jumlah_alpha' => 1,
+                'bulan' => 'Jan',
+                'minggu_per_bulan' => 'Minggu 2',
+                'tahun_ajar_id' => 8,
+                'kelas_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nis' => '201001',
+                'jumlah_hadir' => 1,
+                'jumlah_izin' => 1,
+                'jumlah_sakit' => 1,
+                'jumlah_alpha' => 1,
+                'bulan' => 'Jan',
+                'minggu_per_bulan' => 'Minggu 1',
+                'tahun_ajar_id' => 9,
+                'kelas_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
